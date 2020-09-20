938e633e6470e7eff2e21f88c7e622f197a8a3af
schmelczerandras <andras@schmelzer.dev>
2020-09-20 23:00:09 +0200

    Nicer JSON output

--- src/helper/pretty-print.ts
+++ src/helper/pretty-print.ts
@@ -1,2 +1,4 @@
 export const prettyPrint = (o: any): string =>
-  JSON.stringify(o, (_, v) => (v.toFixed ? Number(v.toFixed(2)) : v), '  ');
+  JSON.stringify(o, (_, v) => (v.toFixed ? Number(v.toFixed(3)) : v), '  ')
+    .replace(/("|,|{|^\n)/g, '')
+    .replace(/(\W*}\n?)+/g, '\n\n');
