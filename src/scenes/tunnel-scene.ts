41ea5d7db97e8a53c5baec074fd78c8e2baba402
schmelczerandras <andras@schmelzer.dev>
2020-10-04 11:25:32 +0200

    Fix non power 2 texture size

--- src/scenes/tunnel-scene.ts
+++ src/scenes/tunnel-scene.ts
@@ -71,7 +71,7 @@ export class TunnelScene implements Scene {
     canvas: HTMLCanvasElement,
     overlay: HTMLDivElement
   ): Promise<void> {
-    const noiseTexture = await renderNoise([1000, 1], 60, 1 / 8);
+    const noiseTexture = await renderNoise([1024, 1], 60, 1 / 8);
 
     this.canvas = canvas;
     this.overlay = overlay;
